6d0550e8c9a279b6491baf67e886f680bd7e8edc
Shaz Balakumar <shbalaku@cisco.com>
2018-02-25 10:19:55 +0000

    headlines function added

--- skills/headlines.js
+++ skills/headlines.js
@@ -12,8 +12,8 @@ module.exports = function (controller) {
         request(api_uri, function (error, response, body) {
           console.log('error:', error); // Print the error if one occurred
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-          console.log('body:', body); // Print the HTML for the Google homepage.
-          bot.reply(message, body);
+          var obj = body.parse(body);
+          bot.reply(message, obj.status);
         });
     });
 }
