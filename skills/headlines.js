98bb12b0446717b70ed308aeee20032eafd271f1
Shaz Balakumar <shbalaku@cisco.com>
2018-02-25 10:12:30 +0000

    headlines test

---
+++ skills/headlines.js
@@ -0,0 +1,15 @@
+//
+// Headlines functionality
+//
+var request = require('request');
+var NEWS_API_KEY = process.env.NEWS_API_KEY;
+
+module.exports = function (controller) {
+
+    controller.hears('headlines', 'direct_mention, direct_message', function (bot, message) {
+
+        var api_uri = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey='+NEWS_API_KEY;
+
+        bot.reply(message, api_uri);
+    });
+}
