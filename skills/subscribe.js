64d7126f4897f5aa14935d22200e6739b44b3304
Shaz Balakumar <shbalaku@cisco.com>
2018-03-06 19:05:19 +0000

    node postgres added

---
+++ skills/subscribe.js
@@ -0,0 +1,26 @@
+//
+// Subscribe functionality
+//
+var request = require('request');
+var methods = require('./../methods.js');
+var NEWS_API_KEY = process.env.NEWS_API_KEY;
+
+module.exports = function (controller) {
+
+    controller.hears('subscribe', 'direct_mention, direct_message', function (bot, message) {
+        
+
+        var api_uri = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey='+NEWS_API_KEY;
+        request(api_uri, function (error, response, body) {
+          var obj = JSON.parse(body);
+          var headlines = obj.articles;
+          var response = '';
+
+          for (var i = 0; i < headlines.length; i++) {
+            article = headlines[i];
+            response = response + '* ' + '**' + article.title + '**' + '\n' + '    * ' + article.description + '\n    * _' + article.url +'_\n';
+          }
+          bot.reply(message, response);
+        });
+    });
+}
