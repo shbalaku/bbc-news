7a51b8b5beba419abadc60d78b42aef0e6615293
Shaz Balakumar <shbalaku@cisco.com>
2018-03-06 19:38:33 +0000

    help update

--- skills/help.js
+++ skills/help.js
@@ -6,6 +6,7 @@ module.exports = function (controller) {
     controller.hears('help', 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
         text += "\n- " + bot.appendMention(message, "headlines") + ": Lists top BBC headlines";
+        text += "\n- " + bot.appendMention(message, "subscribe") + ": Subscribe to top BBC headlines every morning";
         text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
         text += "\n- " + bot.appendMention(message, "show [skill]") + ": display the code of the specified skill";
         bot.reply(message, text);
