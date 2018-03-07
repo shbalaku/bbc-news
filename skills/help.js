b1cb9483e9c9641460100802f586bb185b0c2b9a
Shaz Balakumar <shbalaku@cisco.com>
2018-03-07 16:36:15 +0000

    unsubscribe update

--- skills/help.js
+++ skills/help.js
@@ -6,7 +6,8 @@ module.exports = function (controller) {
     controller.hears('help', 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
         text += "\n- " + bot.appendMention(message, "headlines") + ": Lists top BBC headlines";
-        text += "\n- " + bot.appendMention(message, "subscribe") + ": Subscribe to top BBC headlines every morning";
+        text += "\n- " + bot.appendMention(message, "opt in") + ": Subscribe to top BBC headlines every morning";
+        text += "\n- " + bot.appendMention(message, "opt out") + ": Unsubscribe from top BBC headlines every morning";
         text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
         bot.reply(message, text);
     });
